af5dee1f623487ee5ded6d29729cdf3bf06ece60
wa1ful <plach4747@gmail.com>
2025-12-06 00:19:29 +0300

    asdfasf

--- bot.py
+++ bot.py
@@ -22,6 +22,11 @@ def ban_user(message):
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
 @bot.message_handler(func=lambda message: True)
 def check_messages(message):
     if message.from_user.is_bot:
